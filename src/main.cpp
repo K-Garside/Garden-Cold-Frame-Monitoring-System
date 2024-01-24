352983464802efe93723fbe20a67d7d5d30b1dbd
Kieran Garside <Kieran.Garside@lightning-e.com>
2024-01-24 02:22:27 +0000

    Developed feature B: System now records readings every minute, deleting any onlder than 24hrs
    System now outputs a reading of the current temp and humidity every 5s

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,8 @@
 #include "DHT.h"
+#include <stdio.h>
+#include <time.h>
+#include <iostream>
+#include <vector>
 
 #define DHTPIN 4   
 #define greenLED 21
@@ -7,6 +11,21 @@
 #define DHTTYPE DHT11
 
 DHT dht(DHTPIN, DHTTYPE);
+
+// Assign my epochs and there last changes
+
+const unsigned long DELETION_DELAY = 86400000;
+const unsigned long RECORDING_DELAY = 60000;
+unsigned long RECORDING_LAST;
+const unsigned long SENSOR_DELAY = 1000;
+unsigned long SENSOR_LAST;
+const unsigned long PRINT_DELAY = 5000;
+unsigned long PRINT_LAST;
+
+bool timeDiff(unsigned long start, unsigned long delay)
+{
+  return (millis() - start) >= delay;
+}
 
 class OperatingMode{
   public:
@@ -17,10 +36,111 @@ class OperatingMode{
     float maxHum;
 };
 
-class ColdFrame {
+class Reading {
+  private:
+    float temp;
+    float hum;
+    unsigned long readingTime;
+    OperatingMode mode;
+    bool status;
   public:
+    Reading(bool status, OperatingMode mode)
+    {
+      this->temp = dht.readTemperature();
+      this->hum = dht.readHumidity();
+      this->readingTime = millis();
+      this->mode = mode;
+      this->status = status;
+    }
+
+    bool checkDelete()
+    {
+      if(timeDiff(this->readingTime, DELETION_DELAY))
+      {
+        return true;
+      }
+      else
+      {
+        return false;
+      }
+    }
+};
+
+class ColdFrame {
+  private:
+    float currentTemp;
+    float currentHum;
     OperatingMode mode;
     bool envOk;
+    std::vector<Reading> readings;
+  public:
+    void setTemp(float temp)
+    {
+      this->currentTemp = temp;
+    }
+    float getTemp()
+    {
+      return this->currentTemp;
+    }
+    void setHum(float hum)
+    {
+      this->currentHum = hum;
+    }
+    float getHum()
+    {
+      return this->currentHum;
+    }
+    void setOperatingMode(OperatingMode mode)
+    {
+      this->mode = mode;
+    }
+    OperatingMode getOperatingMode()
+    {
+      return this->mode;
+    }
+    void setEnvStatus(bool envOk)
+    {
+      this->envOk = envOk;
+    }
+
+    bool getEnvStatus()
+    {
+      return this->envOk;
+    }
+    void setLedStatus(bool envStat)
+    {
+        if(envStat != this->envOk)
+        {
+          if(envStat)
+          {
+            digitalWrite(greenLED, HIGH);
+            digitalWrite(redLED, LOW);
+          }
+          else
+          {
+            digitalWrite(redLED, HIGH);
+            digitalWrite(greenLED, LOW);
+          }
+          this->envOk = envStat;
+        }
+    }
+
+    void checkReadings()
+    {
+      for(int i = 0; i < this->readings.size(); i++)
+      {
+        if(this->readings[i].checkDelete())
+        {
+          this->readings.erase(this->readings.begin() + i);
+        }
+      }
+    }
+
+    void addReading()
+    {
+      checkReadings();
+      this->readings.push_back(Reading(this->envOk, this->mode));
+    }
 };
 
 ColdFrame frame;
@@ -30,10 +150,6 @@ OperatingMode Germinating;
 OperatingMode Vegetative;
 OperatingMode Fruiting;
 OperatingMode Night;
-
-void assignOperatingModeVals();
-void setEnvStatus();
-void setLedStatus(bool envOk);
 
 void assignOperatingModeVals()
 {
@@ -63,63 +179,68 @@ void assignOperatingModeVals()
   Night.maxHum = 80;
 }
 
-void setLedStatus(bool envStat)
+void readDHT()
 {
-  if(envStat != frame.envOk)
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+
+  frame.setTemp(t);
+  frame.setHum(h);
+
+  if(t > frame.getOperatingMode().maxTemp || t < frame.getOperatingMode().minTemp || h > frame.getOperatingMode().maxHum || h < frame.getOperatingMode().minHum)
   {
-    if(envStat)
-    {
-      digitalWrite(greenLED, HIGH);
-      digitalWrite(redLED, LOW);
-    }
-    else
-    {
-      digitalWrite(redLED, HIGH);
-      digitalWrite(greenLED, LOW);
-    }
-    frame.envOk = envStat;
+    frame.setLedStatus(false);
   }
-
-
+  else
+  {
+    frame.setLedStatus(true);
+  }
 }
 
-void setup() {
+void printDHT()
+{
+  Serial.print("Current Temp: ");
+  Serial.println(frame.getTemp());
+  Serial.print("Current Hum: ");
+  Serial.println(frame.getHum());
+}
+
+void setup() 
+{
   Serial.begin(115200);
   while(!Serial);
 
   pinMode(greenLED, OUTPUT);
   pinMode(redLED, OUTPUT);
   assignOperatingModeVals();
-  frame.mode = Vegetative;
+  frame.setOperatingMode(Vegetative);
   dht.begin();
-}
 
-void loop() {
-  delay(1000);
   float h = dht.readHumidity();
   float t = dht.readTemperature();
   float f = dht.readTemperature(true);
-
-  // Check if any reads failed and exit early (to try again).
+  
   if (isnan(h) || isnan(t) || isnan(f)) {
     Serial.println(F("Failed to read from DHT sensor"));
-    delay(5000);
     return;
   }
-  setLedStatus(true);
-  // // Compute heat index in Fahrenheit (the default)
-  // float hif = dht.computeHeatIndex(f, h);
-  // // Compute heat index in Celsius (isFahreheit = false)
-  // float hic = dht.computeHeatIndex(t, h, false);
+}
 
-  if(t > frame.mode.maxTemp || t < frame.mode.minTemp || h > frame.mode.maxHum || h < frame.mode.minHum)
+void loop()
+{
+  if(timeDiff(SENSOR_LAST, SENSOR_DELAY))//read sensors -- Might not need to delay this, but just in case
   {
-    setLedStatus(false);
+    SENSOR_LAST = millis();
+    readDHT();
   }
-  else
+  if(timeDiff(RECORDING_LAST, RECORDING_DELAY))//add new record and delete old ones if necessary. Maybe just check to delete old ones every 24hrs instead rather than every time i add a new one, however this also ensures im not adding more than there is volatile memory.
   {
-    setLedStatus(true);
+    RECORDING_LAST = millis();
+    frame.addReading();
   }
-
-
+  if(timeDiff(PRINT_LAST, PRINT_DELAY))//print sensor readings
+  {
+    PRINT_LAST = millis();
+    printDHT();
+  }
 }
